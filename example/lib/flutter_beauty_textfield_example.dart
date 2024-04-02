5d927bb2295fc573214c8cff8c12a0a9cfa5a50f
Prometeus <prometeus@Prometeuss-MacBook-Pro.local>
2024-04-02 16:08:31 +0700

    add

--- example/lib/flutter_beauty_textfield_example.dart
+++ example/lib/flutter_beauty_textfield_example.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_beauty_textfield/flutter_beauty_textfield.dart';
+
+class FlutterBeautyTextfieldExample extends StatefulWidget {
+  const FlutterBeautyTextfieldExample({super.key});
+
+  @override
+  FlutterBeautyTextfieldExampleState createState() =>
+      FlutterBeautyTextfieldExampleState();
+}
+
+class FlutterBeautyTextfieldExampleState
+    extends State<FlutterBeautyTextfieldExample> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+      body: SafeArea(
+        child: FlutterBeautyTextfield(),
+      ),
+    );
+  }
+}
--- lib/src/flutter_beauty_textfield_base.dart
+++ lib/src/flutter_beauty_textfield_base.dart
@@ -1,5 +1,66 @@
 // TODO: Put public facing types in this file.
 
+import 'package:flutter/material.dart';
+
+class FlutterBeautyTextfield extends StatefulWidget {
+  @override
+  FlutterBeautyTextfieldState createState() => FlutterBeautyTextfieldState();
+}
+
+class FlutterBeautyTextfieldState extends State<FlutterBeautyTextfield> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          color: Colors.transparent,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Email",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black87,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 10,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                color: Colors.white,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      )),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
